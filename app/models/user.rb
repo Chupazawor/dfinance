a72ee0135ec801db0fe394ef3865c8575d6baf6d
Igor Chuprinskiy <61902718+Chupazawor@users.noreply.github.com>
2025-01-15 14:48:56 +0300

    add validate to user (#2)
    * add validate to user
    
    * review changes
    
    * review changes 1
    
    * Fix gitignore (#3)
    
    * Fix gitignore
    
    * Rebuild cache

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true, length: { in: 2..20 }
+  validates :email, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+end
---
+++ db/migrate/20250114093714_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.timestamps
+      t.string :first_name, null: false, limit: 20
+      t.string :last_name, null: false, limit: 20
+      t.string :email, null: false, limit: 50
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
